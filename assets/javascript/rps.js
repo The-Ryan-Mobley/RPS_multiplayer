f0af2895383e5a0dd76052899f1af609f8342ecb
Ryan Mobley <ryan.mobley94@gmail.com>
2019-07-27 10:32:25 -0500

    hello world

---
+++ assets/javascript/rps.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD5fyqB6aBN7cPHhByuHSmpY6ERh8cXwh4",
+    authDomain: "rockpaperscissors-d74be.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-d74be.firebaseio.com",
+    projectId: "rockpaperscissors-d74be",
+    storageBucket: "",
+    messagingSenderId: "20615850318",
+    appId: "1:20615850318:web:211a38faf6c6d1dd"
+  };
