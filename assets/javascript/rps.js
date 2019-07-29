31253af84ea764590d99398dc0a7b2a2f697e635
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-28 19:44:48 -0500

    getting firebase set up

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,9 +1,24 @@
+//initialize firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyD5fyqB6aBN7cPHhByuHSmpY6ERh8cXwh4",
-    authDomain: "rockpaperscissors-d74be.firebaseapp.com",
-    databaseURL: "https://rockpaperscissors-d74be.firebaseio.com",
-    projectId: "rockpaperscissors-d74be",
-    storageBucket: "",
-    messagingSenderId: "20615850318",
-    appId: "1:20615850318:web:211a38faf6c6d1dd"
-  };
+  apiKey: "AIzaSyD5fyqB6aBN7cPHhByuHSmpY6ERh8cXwh4",
+  authDomain: "rockpaperscissors-d74be.firebaseapp.com",
+  databaseURL: "https://rockpaperscissors-d74be.firebaseio.com",
+  projectId: "rockpaperscissors-d74be",
+  storageBucket: "",
+  messagingSenderId: "20615850318",
+  appId: "1:20615850318:web:211a38faf6c6d1dd"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+//jquery elements
+const rock=$('#rock');
+const papaer =$('#paper');
+const scissors =$('#scissors');
+const play_button =$('#play');
+
+var testvar ="hello world";
+
+firebase.ref().set({
+  testing: testvar,
+
+});
