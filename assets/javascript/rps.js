2595a993adb1e9c6e1cc10884b1ee849054ade4c
Ryan Mobley <ryan.mobley94@gmail.com>
2019-07-31 10:41:50 -0500

    working on user count

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -11,27 +11,111 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 //jquery elements
-const rock=$('#rock');
-const papaer =$('#paper');
-const scissors =$('#scissors');
-const play_button =$('#play');
+const selection_point = $('.selection-point');
+const play_button = $('#play');
+const join_button = $('#join');
 
-var testvar ="hello world";
+var testvar = "hello world";
+var has_playerone = false;
+var has_playertwo = false;
+var users = 0;
+function rps(pone_choice,ptwo_choice){
+  if(pone_choice === 'rock'){
+    if(ptwo_choice === 'rock'){
+
+    }
+    else if(ptwo_choice === 'paper'){
+
+    }
+    else{
+
+    }
+  }
+  if(pone_choice === 'peper'){
+    if(ptwo_choice === 'rock'){
+
+    }
+    else if(ptwo_choice === 'paper'){
+
+    }
+    else{
+      
+    }
+  }
+  if(pone_choice === 'scissors'){
+    if(ptwo_choice === 'rock'){
+
+    }
+    else if(ptwo_choice === 'paper'){
+
+    }
+    else{
+      
+    }
+  }
+}
 
 database.ref().set({
   testing: testvar,
+  pone: has_playerone,
+  ptwo: has_playertwo,
+  usernum: users
 
 });
 var connectionsRef = database.ref('/connections');
 var connectedRef = database.ref('.info/connected');
-connectedRef.on('value', (snap)=>{
-  if(snap.val()){
-    let con = connectionsRef.push(true)
-    con.onDisconnect().remove();
+// connectionsRef.once('value', function (snapshot) {
+//   if (Object.keys(snapshot.val()).indexOf('1') === -1) {
+//       player.number = '1';
+//       opponent.number = '2';
+//   } else if (Object.keys(snapshot.val()).indexOf('2') === -1) {
+//       player.number = '2';
+//       opponent.number = '1';
+//   }
+// ​
+//   if (player.number !== '0') {
+// ​
+//       con = connectionsRef.child(player.number);
+//       con.set(player);
+// ​
+//       con.onDisconnect().remove();
+// ​
+//   } else {
+//       app.delete();
+//   }
+//   console.log("You are Player " + player.number)
+//   console.log("You are Opponent " + opponent.number)
+// });
+// ​
+// if (player.number = '1') {
+//   $(".player-two-form").hide();
+// } else if(opponent.number = '1'){
+//   $(".player-one-form").hide();
+// }
+// else{}
+var connectionsRef = database.ref('/connections');
+var connectedRef = database.ref('.info/connected');
+connectedRef.on('value', (snap) => {
+  if (users < 2) {
+    if (snap.val()) {
+      let con = connectionsRef.push(true)
+      con.onDisconnect().remove();
+    }
+    users++;
   }
 
 });
-connectionsRef.on('value',(snapshot)=>{
+connectionsRef.on('value', (snapshot) => {
   console.log(snapshot.numChildren());
 
 })
+database.ref().on('value', (snapshot) => {
+  console.log(snapshot.val());
+
+});
+join_button.on('click', (event) => {
+
+});
+selection_point.on('click', (element)=>{
+
+});
