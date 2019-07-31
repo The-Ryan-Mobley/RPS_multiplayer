352f20ee8ce39d5740e7d994d752b37dcedc51f3
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-31 18:12:18 -0500

    added game logic beginning to impliment database storage

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -16,106 +16,156 @@ const play_button = $('#play');
 const join_button = $('#join');
 
 var testvar = "hello world";
-var has_playerone = false;
-var has_playertwo = false;
+var playerone_clicked = false;
+var playertwo_clicked = false;
 var users = 0;
-function rps(pone_choice,ptwo_choice){
-  if(pone_choice === 'rock'){
-    if(ptwo_choice === 'rock'){
 
-    }
-    else if(ptwo_choice === 'paper'){
-
-    }
-    else{
-
-    }
+class user {
+  constructor() {
+    this.number = 0;
+    this.name = "";
+    this.wins = 0;
+    this.losses = 0;
+    this.turns = 0;
+    this.choice = "";
   }
-  if(pone_choice === 'peper'){
-    if(ptwo_choice === 'rock'){
+}
+var player = new user();
+var opponent = new user();
 
-    }
-    else if(ptwo_choice === 'paper'){
-
-    }
-    else{
-      
-    }
+function round_calc(){
+  if (playerone_clicked && playertwo_clicked) {
+    rps(player.choice,opponent.choice);
   }
-  if(pone_choice === 'scissors'){
-    if(ptwo_choice === 'rock'){
+}
+function rps(pone_choice, ptwo_choice) {
+  
+    if (pone_choice === 'rock') {
+      if (ptwo_choice === 'rock') {
+        let chat_line = $('<p>');
+        chat_line.html('DRAW!');
+        chat_line.appendTo($('.chat'));
+        player.turns++;
+        opponent.turns++;
 
-    }
-    else if(ptwo_choice === 'paper'){
+      } else if (ptwo_choice === 'paper') {
+        let chat_line = $('<p>');
+        chat_line.html('Paper Covers Rock!');
+        chat_line.appendTo($('.chat'));
+        player.losses++;
+        opponent.wins++;
+        player.turns++;
+        opponent.turns++;
 
+      } else {
+        let chat_line = $('<p>');
+        chat_line.html('Rock Beats Scissors!');
+        chat_line.appendTo($('.chat'));
+        player.wins++;
+        opponent.losses++;
+        player.turns++;
+        opponent.turns++;
+
+      }
     }
-    else{
-      
+    if (pone_choice === 'paper') {
+      if (ptwo_choice === 'rock') {
+        let chat_line = $('<p>');
+        chat_line.html('Paper Covers Rock!');
+        chat_line.appendTo($('.chat'));
+        player.wins++;
+        opponent.losses++;
+        player.turns++;
+        opponent.turns++;
+
+      } else if (ptwo_choice === 'paper') {
+        let chat_line = $('<p>');
+        chat_line.html('DRAW!');
+        chat_line.appendTo($('.chat'));
+        player.turns++;
+        opponent.turns++;
+
+      } else {
+        let chat_line = $('<p>');
+        chat_line.html('Scissors Cuts Paper!');
+        chat_line.appendTo($('.chat'));
+        player.losses++;
+        opponent.wins++;
+        player.turns++;
+        opponent.turns++;
+
+      }
+    }
+    if (pone_choice === 'scissors') {
+      if (ptwo_choice === 'rock') {
+        let chat_line = $('<p>');
+        chat_line.html('Rock Beats Scissors!');
+        chat_line.appendTo($('.chat'));
+        player.losses++;
+        opponent.wins++;
+        player.turns++;
+        opponent.turns++;
+
+      } else if (ptwo_choice === 'paper') {
+
+      } else {
+
+      }
     }
   }
 }
 
-database.ref().set({
-  testing: testvar,
-  pone: has_playerone,
-  ptwo: has_playertwo,
-  usernum: users
 
-});
-var connectionsRef = database.ref('/connections');
-var connectedRef = database.ref('.info/connected');
-// connectionsRef.once('value', function (snapshot) {
-//   if (Object.keys(snapshot.val()).indexOf('1') === -1) {
-//       player.number = '1';
-//       opponent.number = '2';
-//   } else if (Object.keys(snapshot.val()).indexOf('2') === -1) {
-//       player.number = '2';
-//       opponent.number = '1';
-//   }
-// ​
-//   if (player.number !== '0') {
-// ​
-//       con = connectionsRef.child(player.number);
-//       con.set(player);
-// ​
-//       con.onDisconnect().remove();
-// ​
-//   } else {
-//       app.delete();
-//   }
-//   console.log("You are Player " + player.number)
-//   console.log("You are Opponent " + opponent.number)
-// });
-// ​
-// if (player.number = '1') {
-//   $(".player-two-form").hide();
-// } else if(opponent.number = '1'){
-//   $(".player-one-form").hide();
-// }
-// else{}
 var connectionsRef = database.ref('/connections');
 var connectedRef = database.ref('.info/connected');
 connectedRef.on('value', (snap) => {
-  if (users < 2) {
-    if (snap.val()) {
-      let con = connectionsRef.push(true)
-      con.onDisconnect().remove();
-    }
-    users++;
+  if (snap.val()) {
+    let con = connectionsRef.push(true);
+    con.onDisconnect().remove();
   }
 
 });
-connectionsRef.on('value', (snapshot) => {
-  console.log(snapshot.numChildren());
+connectionsRef.once('value', function (snapshot) {
+  if (Object.keys(snapshot.val()).indexOf('1') === -1) {
+    player.number = '1';
+    opponent.number = '2';
+  } else if (Object.keys(snapshot.val()).indexOf('2') === -1) {
+    player.number = '2';
+    opponent.number = '1';
+  }
+  if (player.number !== '0') {
+    con = connectionsRef.child(player.number);
+    con.set(player);
+    con.onDisconnect().remove();
+  } else {
+    app.delete();
+  }
+  console.log("You are Player " + player.number)
+  console.log("You are Opponent " + opponent.number)
+});
+if (player.number = '1') {
+  $(".player-two-form").hide();
+} else if (opponent.number = '1') {
+  $(".player-one-form").hide();
+}
 
-})
 database.ref().on('value', (snapshot) => {
   console.log(snapshot.val());
 
 });
-join_button.on('click', (event) => {
+
+$('.player-one-form').on('click', '.selection-point', (event) => {
+  let clicked = $('.selections').find(event.target);
+  console.log(clicked.data('name'));
+  player.choice = clicked.data('name');
+  playerone_clicked = true;
+  
 
 });
-selection_point.on('click', (element)=>{
+$('.player-two-form').on('click', '.selection-point', (event) => {
+  let clicked = $('.selections').find(event.target);
+  console.log(clicked.data('name'));
+  opponent.choice = clicked.data('name');
+  playertwo_clicked = true;
 
 });
