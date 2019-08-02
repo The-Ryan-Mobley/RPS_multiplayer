f631293ad21e2dcdc6a51e53115f921b4bfbb4eb
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-01 23:16:06 -0500

    logic

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -15,14 +15,15 @@ const selection_point = $('.selection-point');
 const play_button = $('#play');
 const join_button = $('#join');
 
-var testvar = "hello world";
+
 var playerone_clicked = false;
 var playertwo_clicked = false;
 var users = 0;
+var con = undefined;
 
 class user {
   constructor() {
-    this.number = 0;
+    this.number = "";
     this.name = "";
     this.wins = 0;
     this.losses = 0;
@@ -33,86 +34,87 @@ class user {
 var player = new user();
 var opponent = new user();
 
-function round_calc(){
+function round_calc() {
   if (playerone_clicked && playertwo_clicked) {
-    rps(player.choice,opponent.choice);
+    rps(player.choice, opponent.choice);
   }
 }
+
 function rps(pone_choice, ptwo_choice) {
-  
-    if (pone_choice === 'rock') {
-      if (ptwo_choice === 'rock') {
-        let chat_line = $('<p>');
-        chat_line.html('DRAW!');
-        chat_line.appendTo($('.chat'));
-        player.turns++;
-        opponent.turns++;
 
-      } else if (ptwo_choice === 'paper') {
-        let chat_line = $('<p>');
-        chat_line.html('Paper Covers Rock!');
-        chat_line.appendTo($('.chat'));
-        player.losses++;
-        opponent.wins++;
-        player.turns++;
-        opponent.turns++;
+  if (pone_choice === 'rock') {
+    if (ptwo_choice === 'rock') {
+      let chat_line = $('<p>');
+      chat_line.html('DRAW!');
+      chat_line.appendTo($('.chat'));
+      player.turns++;
+      opponent.turns++;
 
-      } else {
-        let chat_line = $('<p>');
-        chat_line.html('Rock Beats Scissors!');
-        chat_line.appendTo($('.chat'));
-        player.wins++;
-        opponent.losses++;
-        player.turns++;
-        opponent.turns++;
+    } else if (ptwo_choice === 'paper') {
+      let chat_line = $('<p>');
+      chat_line.html('Paper Covers Rock!');
+      chat_line.appendTo($('.chat'));
+      player.losses++;
+      opponent.wins++;
+      player.turns++;
+      opponent.turns++;
 
-      }
-    }
-    if (pone_choice === 'paper') {
-      if (ptwo_choice === 'rock') {
-        let chat_line = $('<p>');
-        chat_line.html('Paper Covers Rock!');
-        chat_line.appendTo($('.chat'));
-        player.wins++;
-        opponent.losses++;
-        player.turns++;
-        opponent.turns++;
+    } else {
+      let chat_line = $('<p>');
+      chat_line.html('Rock Beats Scissors!');
+      chat_line.appendTo($('.chat'));
+      player.wins++;
+      opponent.losses++;
+      player.turns++;
+      opponent.turns++;
 
-      } else if (ptwo_choice === 'paper') {
-        let chat_line = $('<p>');
-        chat_line.html('DRAW!');
-        chat_line.appendTo($('.chat'));
-        player.turns++;
-        opponent.turns++;
-
-      } else {
-        let chat_line = $('<p>');
-        chat_line.html('Scissors Cuts Paper!');
-        chat_line.appendTo($('.chat'));
-        player.losses++;
-        opponent.wins++;
-        player.turns++;
-        opponent.turns++;
-
-      }
-    }
-    if (pone_choice === 'scissors') {
-      if (ptwo_choice === 'rock') {
-        let chat_line = $('<p>');
-        chat_line.html('Rock Beats Scissors!');
-        chat_line.appendTo($('.chat'));
-        player.losses++;
-        opponent.wins++;
-        player.turns++;
-        opponent.turns++;
-
-      } else if (ptwo_choice === 'paper') {
-
-      } else {
-
-      }
     }
   }
+  if (pone_choice === 'paper') {
+    if (ptwo_choice === 'rock') {
+      let chat_line = $('<p>');
+      chat_line.html('Paper Covers Rock!');
+      chat_line.appendTo($('.chat'));
+      player.wins++;
+      opponent.losses++;
+      player.turns++;
+      opponent.turns++;
+
+    } else if (ptwo_choice === 'paper') {
+      let chat_line = $('<p>');
+      chat_line.html('DRAW!');
+      chat_line.appendTo($('.chat'));
+      player.turns++;
+      opponent.turns++;
+
+    } else {
+      let chat_line = $('<p>');
+      chat_line.html('Scissors Cuts Paper!');
+      chat_line.appendTo($('.chat'));
+      player.losses++;
+      opponent.wins++;
+      player.turns++;
+      opponent.turns++;
+
+    }
+  }
+  if (pone_choice === 'scissors') {
+    if (ptwo_choice === 'rock') {
+      let chat_line = $('<p>');
+      chat_line.html('Rock Beats Scissors!');
+      chat_line.appendTo($('.chat'));
+      player.losses++;
+      opponent.wins++;
+      player.turns++;
+      opponent.turns++;
+
+    } else if (ptwo_choice === 'paper') {
+
+    } else {
+
+    }
+  }
+}
 
 
 
@@ -137,6 +139,10 @@ connectionsRef.once('value', function (snapshot) {
     con = connectionsRef.child(player.number);
     con.set(player);
     con.onDisconnect().remove();
+    let statuslog = $('<p>');
+    statuslog.html('player joined');
+    $('chat').append(statuslog);
+    
   } else {
     app.delete();
   }
@@ -145,38 +151,25 @@ connectionsRef.once('value', function (snapshot) {
 });
 if (player.number = '1') {
   $(".player-two-form").hide();
-} else if (opponent.number = '1') {
-  $(".player-one-form").hide();
+  
 }
 
-database.ref().on('value', (snapshot) => {
-  console.log(snapshot.val());
+connectionsRef.on('value', function (snapshot) {
+  if (con) {
+    if (Object.keys(snapshot.val()).indexOf(opponent.number) !== -1) {
+      opponent = snapshot.val()[opponent.number];
+      player = snapshot.val()[player.number];
+    }
+  }
+})
 
-});
 
 $('.player-one-form').on('click', '.selection-point', (event) => {
   let clicked = $('.selections').find(event.target);
   console.log(clicked.data('name'));
   player.choice = clicked.data('name');
   playerone_clicked = true;
-  database.ref().set({
-    playerChoice: player.choice,
-    playerClicked: playerone_clicked,
-
-  });
-  round_calc();
-
-});
-$('.player-two-form').on('click', '.selection-point', (event) => {
-  let clicked = $('.selections').find(event.target);
-  console.log(clicked.data('name'));
-  opponent.choice = clicked.data('name');
-  playertwo_clicked = true;
-  database.ref().set({
-    opponentChoice: opponent.choice,
-    opponentClicked: playertwo_clicked,
-
-  });
+  
   round_calc();
 
 });
