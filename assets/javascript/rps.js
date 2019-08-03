2d5e17b60f14dad551d3105f63733d06f84e8e76
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-02 20:44:46 -0500

    deploying

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -15,29 +15,24 @@ const selection_point = $('.selection-point');
 const play_button = $('#play');
 const join_button = $('#join');
 
+var con = undefined; //used later to check connection
 
-var playerone_clicked = false;
-var playertwo_clicked = false;
-var users = 0;
-var con = undefined;
-
-class user {
+class user { //player object
   constructor() {
     this.number = "";
     this.wins = 0;
     this.losses = 0;
-    this.turns = 0;
     this.choice = "";
     this.clicked = false;
   }
 }
-var player = new user();
-var opponent = new user();
+var player = new user(); //player 1
+var opponent = new user(); //player 2
 
 const connectionsRef = database.ref('/connections');
 const connectedRef = database.ref('.info/connected');
 const chatRef = database.ref('/chat');
-connectedRef.on('value', (snap) => {
+connectedRef.on('value', (snap) => { //push connection and remove on idsconnect
   if (snap.val()) {
     let connected = connectionsRef.push(true);
     connected.onDisconnect().remove();
@@ -45,7 +40,7 @@ connectedRef.on('value', (snap) => {
   }
 
 });
-connectionsRef.once('value', function (snapshot) {
+connectionsRef.once('value', function (snapshot) { //assign player 1 and 2 or 'opponent'
   if (Object.keys(snapshot.val()).indexOf('1') === -1) {
     player.number = '1';
     opponent.number = '2';
@@ -60,13 +55,10 @@ connectionsRef.once('value', function (snapshot) {
     let statuslog = $('<p>');
     $('.chat').append(statuslog);
     statuslog.html('player joined');
-    
-    
+
   } else {
     app.delete();
   }
-  console.log("You are Player " + player.number)
-  console.log("You are Opponent " + opponent.number)
 });
 
 connectionsRef.on('value', function (snapshot) {
@@ -75,65 +67,53 @@ connectionsRef.on('value', function (snapshot) {
       opponent = snapshot.val()[opponent.number];
       player = snapshot.val()[player.number];
     }
-    
-    
-    
   }
-  
+
 });
 
-function round_calc() {
-  connectionsRef.once('value',(snapshot)=>{
-    if((snapshot.val()[player.number].clicked === true) && (snapshot.val()[opponent.number].clicked === true)){
-      console.log('trigger');
-      rps(snapshot.val()[player.number].choice,snapshot.val()[opponent.number].choice);
+function round_calc() { //rock paper scissors game logic after checking if both users have clicked an option
+  connectionsRef.once('value', (snapshot) => {
+    if ((Object.keys(snapshot.val()).indexOf('1') !== -1) && (Object.keys(snapshot.val()).indexOf('2') !== -1)) {
+      if ((snapshot.val()[player.number].clicked === true) && (snapshot.val()[opponent.number].clicked === true)) {
+        rps(snapshot.val()[player.number].choice, snapshot.val()[opponent.number].choice);
 
+      }
     }
 
   });
-  
-  
-  
-
 }
-chatRef.on('child_added',(childsnap)=>{
-  console.log(childsnap.val());
+
+chatRef.on('child_added', (childsnap) => { //appends chat
   let chattext = childsnap.val();
-  
+
   let newchat = $('<p>');
   $('.chat').append(childsnap.val().chatline + '<br>');
-  
-  
-
 
 });
-function set_next_round(){
-  console.log(player.wins);
-  
-  connectionsRef.child('1').update({"/choice": ""});
-  connectionsRef.child('1').update({"/clicked": false});
-  
-  connectionsRef.child('2').update({"/choice": ""});
-  connectionsRef.child('2').update({"/clicked": false});
-  
 
+function set_next_round() { //updates for next round
+  connectionsRef.child('1').update({
+    "/choice": ""
+  });
+  connectionsRef.child('1').update({
+    "/clicked": false
+  });
+
+  connectionsRef.child('2').update({
+    "/choice": ""
+  });
+  connectionsRef.child('2').update({
+    "/clicked": false
+  });
 }
-  
 
 function rps(pone_choice, ptwo_choice) {
 
-  if (pone_choice === 'rock') {
+  if (pone_choice === 'rock') { //checks choices and updates win/loss
     if (ptwo_choice === 'rock') {
       let chat_line = $('<p>');
       chat_line.appendTo($('.chat'));
       chat_line.html('DRAW!');
-      player.turns++;
-      opponent.turns++;
-      console.log(player.wins);
-      connectionsRef.child('1').update({"/wins": player.wins});
-      connectionsRef.child('2').update({"/losses": opponent.losses});
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
 
     } else if (ptwo_choice === 'paper') {
       let chat_line = $('<p>');
@@ -141,13 +121,12 @@ function rps(pone_choice, ptwo_choice) {
       chat_line.html('Paper Covers Rock!');
       player.losses++;
       opponent.wins++;
-      player.turns++;
-      opponent.turns++;
-      connectionsRef.child('2').update({"/wins": opponent.wins});
-      connectionsRef.child('1').update({"/losses": player.losses})
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
-      
+      connectionsRef.child('2').update({
+        "/wins": opponent.wins
+      });
+      connectionsRef.child('1').update({
+        "/losses": player.losses
+      })
 
     } else {
       let chat_line = $('<p>');
@@ -155,13 +134,12 @@ function rps(pone_choice, ptwo_choice) {
       chat_line.html('Rock Beats Scissors!');
       player.wins++;
       opponent.losses++;
-      player.turns++;
-      opponent.turns++;
-      console.log(player.wins);
-      connectionsRef.child('1').update({"/wins": player.wins});
-      connectionsRef.child('2').update({"/losses": opponent.losses});
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
+      connectionsRef.child('1').update({
+        "/wins": player.wins
+      });
+      connectionsRef.child('2').update({
+        "/losses": opponent.losses
+      });
 
     }
   }
@@ -172,22 +150,17 @@ function rps(pone_choice, ptwo_choice) {
       chat_line.html('Paper Covers Rock!');
       player.wins++;
       opponent.losses++;
-      player.turns++;
-      opponent.turns++;
-      console.log(player.wins);
-      connectionsRef.child('1').update({"/wins": player.wins});
-      connectionsRef.child('2').update({"/losses": opponent.losses});
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
+      connectionsRef.child('1').update({
+        "/wins": player.wins
+      });
+      connectionsRef.child('2').update({
+        "/losses": opponent.losses
+      });
 
     } else if (ptwo_choice === 'paper') {
       let chat_line = $('<p>');
       chat_line.appendTo($('.chat'));
       chat_line.html('DRAW!');
-      player.turns++;
-      opponent.turns++;
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
 
     } else {
       let chat_line = $('<p>');
@@ -195,53 +168,46 @@ function rps(pone_choice, ptwo_choice) {
       chat_line.html('Scissors Cuts Paper!');
       player.losses++;
       opponent.wins++;
-      player.turns++;
-      opponent.turns++;
-      connectionsRef.child('2').update({"/wins": opponent.wins});
-      connectionsRef.child('1').update({"/losses": player.losses});
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
+      connectionsRef.child('2').update({
+        "/wins": opponent.wins
+      });
+      connectionsRef.child('1').update({
+        "/losses": player.losses
+      });
 
     }
   }
   if (pone_choice === 'scissors') {
     if (ptwo_choice === 'rock') {
       let chat_line = $('<p>');
-      
       chat_line.appendTo($('.chat'));
       chat_line.html('Rock Beats Scissors!');
       player.losses++;
       opponent.wins++;
-      player.turns++;
-      opponent.turns++;
-      connectionsRef.child('2').update({"/wins": opponent.wins});
-      connectionsRef.child('1').update({"/losses": player.losses});
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
+      connectionsRef.child('2').update({
+        "/wins": opponent.wins
+      });
+      connectionsRef.child('1').update({
+        "/losses": player.losses
+      });
 
     } else if (ptwo_choice === 'paper') {
       let chat_line = $('<p>');
-      
       chat_line.appendTo($('.chat'));
       chat_line.html('Scissors Cuts Paper!');
       player.wins++;
       opponent.losses++;
-      player.turns++;
-      opponent.turns++;
-      console.log(player.wins);
-      connectionsRef.child('1').update({"/wins": player.wins});
-      connectionsRef.child('2').update({"/losses": opponent.losses});
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
+      connectionsRef.child('1').update({
+        "/wins": player.wins
+      });
+      connectionsRef.child('2').update({
+        "/losses": opponent.losses
+      });
 
     } else {
       let chat_line = $('<p>');
       chat_line.appendTo($('.chat'));
       chat_line.html('DRAW!');
-      player.turns++;
-      opponent.turns++;
-      connectionsRef.child('1').update({"/turns": player.turns});
-      connectionsRef.child('2').update({"/turns": opponent.turns});
 
     }
   }
@@ -252,42 +218,45 @@ function rps(pone_choice, ptwo_choice) {
 
 
 
-connectionsRef.on('value',(snapshot)=>{ //tracks win loss
-  if((Object.keys(snapshot.val()).indexOf('1') !== -1)&&(Object.keys(snapshot.val()).indexOf('2') !== -1)){
-    $('#wins-p').html('WINS: '+ snapshot.val()[player.number].wins);
-    $('#losses-p').html('LOSSES: '+ snapshot.val()[player.number].losses);
-    $('#wins-o').html('WINS: '+ snapshot.val()[opponent.number].wins);
-    $('#losses-o').html('LOSSES: '+ snapshot.val()[opponent.number].losses);
+connectionsRef.on('value', (snapshot) => { //tracks win loss html elements
+  if ((Object.keys(snapshot.val()).indexOf('1') !== -1) && (Object.keys(snapshot.val()).indexOf('2') !== -1)) {
+    $('#wins-p').html('WINS: ' + snapshot.val()[player.number].wins);
+    $('#losses-p').html('LOSSES: ' + snapshot.val()[player.number].losses);
+    $('#wins-o').html('WINS: ' + snapshot.val()[opponent.number].wins);
+    $('#losses-o').html('LOSSES: ' + snapshot.val()[opponent.number].losses);
   }
 });
-$('.player-one-form').on('click', '.pic-select', (event) => {
+$('.player-one-form').on('click', '.pic-select', (event) => { //click event to get value
   let clicked = $('.selections').find(event.target);
-  
-  connectionsRef.once('value', ()=> {
-    
-    if(con.path.pieces_[1] === '1'){
+  connectionsRef.once('value', () => {
+
+    if (con.path.pieces_[1] === '1') {
       player.choice = clicked.data('name');
-      connectionsRef.child('1').update({"/choice": player.choice});
-      connectionsRef.child('1').update({"/clicked": true});
-    }else{
+      connectionsRef.child('1').update({
+        "/choice": player.choice
+      });
+      connectionsRef.child('1').update({
+        "/clicked": true
+      });
+    } else {
       opponent.choice = clicked.data('name');
-      connectionsRef.child('2').update({"/choice": opponent.choice});
-      connectionsRef.child('2').update({"/clicked": true});
+      connectionsRef.child('2').update({
+        "/choice": opponent.choice
+      });
+      connectionsRef.child('2').update({
+        "/clicked": true
+      });
     }
-    
-    
-    
+
   });
   round_calc();
-  
 
 });
-$('#chat-button').on('click',()=>{
-  
-  
+$('#chat-button').on('click', () => {
   chatRef.push({
     chatline: $('#chat-line').val(),
 
   });
+  $('#chat-button').val("");
 
 });
